3f92026083ddb8465aa1054c8615293ca4469e3e
Teun Pronk <tpronk@convenient.nl>
2019-07-03 11:52:23 +0200

    Added SparkJwtAuthorizationAttribute

---
+++ src/Spark/Authentication/SparkJwtAuthorizationAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Text;
+using System.Security.Claims;
+using System.Net.Http.Headers;
+
+namespace Spark.Authentication
+{
+    public class SparkJwtAuthorizationAttribute : AuthorizeAttribute
+    {
+        private readonly SparkJwtSecurityTokenHandler tokenHandler = new SparkJwtSecurityTokenHandler();
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            try
+            {
+                string sToken = actionContext.Request.Headers.Authorization.Parameter;
+                ClaimsPrincipal res = tokenHandler.DoValidateToken(sToken, out SecurityToken outValue);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
